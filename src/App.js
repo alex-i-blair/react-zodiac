4843f8630a23ad53653bbd62361e785bea28886b
Alex Blair <bclan11@gmail.com>
2022-01-24 17:55:27 -0800

    init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css'
-{/* import the zodiac array */}
+// import { ZodiacCard } from './ZodiacCard/ZodiacCard'
+import { zodiac } from './data'
+import Header from './Header/Header'
+import Footer from './Footer/Footer'
+import Main from './Main/Main'
+/* import the zodiac array */
+{
+  /* import and use the Header. Main, and Footer components here */
+}
 
 function App() {
   return (
     <div className="App">
-      {/* import and use the Header. Main, and Footer components here */}
+      <Header firstName="Alex" lastName="Blair"/>
       {/* the Header component needs a string passed in as the `name` prop */}
+      <Main zodiacs={zodiac} />
       {/* the Footer component needs a year passed in as the `year` prop */}
       {/* pass the zodiac array imported above to the Main component as a `zodiacs` prop */}
+      <Footer year='3045' />
     </div>
   )
 }
--- src/ZodiacCard/ZodiacCard.js
+++ src/ZodiacCard/ZodiacCard.js
@@ -1,5 +1,17 @@
+import { Fragment } from 'react/cjs/react.production.min'
 import './ZodiacCard.css'
 
-export default function ZodiacCard() {
-  {/* in this component,  use the `name` and `dates` props to render the name and dates of the sign, as well as load the correct image  from the public directory */}
+export default function ZodiacCard(props) {
+  return (
+    <fragment className='zodiac-card'>
+      <p>
+        {/* {console.log(props.name)} */}
+        {props.name.charAt(0).toUpperCase() + props.name.slice(1) + ' ' + props.dates}
+      </p>
+      <img src={`./images/${props.name}.png`} />
+    </fragment>
+  )
+}
+{
+  /* in this component,  use the `name` and `dates` props to render the name and dates of the sign, as well as load the correct image  from the public directory */
 }
